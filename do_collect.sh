8d54eba2aaa726984a0a15caeca13a3051af0699
Robert Nurnberg <robert.nurnberg@gmx.de>
2025-05-14 16:15:46 +0200

    only poll trimmed version

--- do_collect.sh
+++ do_collect.sh
@@ -3,21 +3,42 @@
 # exit on errors
 set -e
 
-temp_file="_tmp_eco_cdbpv.epd"
+PREFIX="eco"
+SOURCE="${PREFIX}.epd"
+DEST="${PREFIX}_cdbpv.epd"
+TEMP_FILE="_tmp_$DEST"
+TRIMMED="${PREFIX}_trimmed.epd"
+MATES="${PREFIX}_mates.epd"
+ORACLE="../caissatrack/caissa_sorted_100000_cdbpv.epd $MATES"
+FILTER="python ../cdblib/addons/fens_filter_overlap.py --saveMemory --noStats"
+CDBBULK="python ../cdblib/cdbbulkpv.py -s -c 8 --stable --user rob"
+SCORE="python ../cdblib/addons/score_fens_locally.py"
 
-if [ -f "$temp_file" ]; then
-    echo "$temp_file already exists. Exiting."
+if [ -f "$TEMP_FILE" ]; then
+    echo "$TEMP_FILE already exists. Exiting."
     exit 0
 fi
 
-python ../cdblib/cdbbulkpv.py -s -c 8 --stable --user rob eco.epd >"$temp_file"
+if [ -f "$DEST" ]; then
+    grep mated "$DEST" >"$MATES"
+else
+    rm -f "$MATES"
+    touch "$MATES"
+fi
 
-mv "$temp_file" eco_cdbpv.epd
+$FILTER $SOURCE $ORACLE >"$TRIMMED"
+$CDBBULK "$TRIMMED" >"$TEMP_FILE"
+$SCORE $SOURCE $TEMP_FILE $ORACLE >"$DEST"
+rm "$TEMP_FILE"
 
-python ../caissatrack/caissatrack.py eco_cdbpv.epd >>ecotrack.csv
-python ../caissatrack/extract_fens.py eco_cdbpv.epd --shortest 100 --ignore2folds >eco_daily_shortest.epd
-python ../caissatrack/extract_fens.py eco_cdbpv.epd --evalMin 90 --evalMax 105 >eco_daily_edgy.epd
+CSV="${PREFIX}track.csv"
+SHORTEST="${PREFIX}_daily_shortest.epd"
+EDGY="${PREFIX}_daily_edgy.epd"
 
-git add eco_cdbpv.epd ecotrack.csv eco_daily_shortest.epd eco_daily_edgy.epd
+python ../caissatrack/caissatrack.py "$DEST" >>"$CSV"
+python ../caissatrack/extract_fens.py "$DEST" --shortest 100 --ignore2folds >"$SHORTEST"
+python ../caissatrack/extract_fens.py "$DEST" --evalMin 90 --evalMax 105 >"$EDGY"
+
+git add "$DEST" "$CSV" "$SHORTEST" "$EDGY"
 git diff --staged --quiet || git commit -m "update data"
 git push origin main >&push.log
